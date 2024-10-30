de19add5503e35de81b59e00d3f2483f30c81393
Wiktor-Jordeczka <135605897+Wiktor-Jordeczka@users.noreply.github.com>
2024-10-30 17:02:16 +0100

    Remove Demo

--- backend/src/main/java/pl/poznan/put/planner_endpoints/demo/Demo.java
+++
@@ -1,8 +0,0 @@
-package pl.poznan.put.planner_endpoints.Demo;
-
-/**
- * Simple resource class for demonstration
- * @param text Returned text
- * @param lp sequence number
- */
-public record Demo (String text, Integer lp) { }
